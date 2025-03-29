5d417fd23133d0b46ab11dc548bd823061168cbe
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2025-03-29 16:07:55 +0600

    Finished HomeHero

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,31 +1,27 @@
-import React, { FC, use } from 'react';
-import { setRequestLocale } from 'next-intl/server';
-import { routing } from '@/i18n/routing';
+import React, { FC, use } from "react";
+import { setRequestLocale } from "next-intl/server";
+import { routing } from "@/i18n/routing";
 
 // Sections
-import PortfolioHero from '@/components/portfolio/PortfolioHero';
-import PhotoGallery from '@/components/portfolio/PhotoGallery';
-import AboutPawisCrea from '@/components/portfolio/AboutPawisCrea';
+import HomeHero from "@/components/home/HomeHero";
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
 }
 
-interface PortfolioPageProps {
+interface HomePageProps {
   params: Promise<{ locale: string }>;
 }
 
-const PortfolioPage: FC<PortfolioPageProps> = ({ params }) => {
+const HomePage: FC<HomePageProps> = ({ params }) => {
   const { locale } = use(params);
   setRequestLocale(locale);
 
   return (
-    <main className="portfolio-page">
-      <PortfolioHero />
-      {/* <PhotoGallery /> */}
-      <AboutPawisCrea />
+    <main className="home-page">
+      <HomeHero />
     </main>
   );
 };
 
-export default PortfolioPage;
+export default HomePage;
