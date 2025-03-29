394ebc25029e2df98c8437f3e5f22ac203a03c27
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2025-03-29 19:44:36 +0600

    Finished PortfolioHero

--- components/portfolio/PortfolioHero.tsx
+++ components/portfolio/PortfolioHero.tsx
@@ -1,52 +1,77 @@
-import React, { FC } from 'react';
-import { useTranslations } from 'next-intl';
-import { Link } from '@/i18n/navigation';
+import React, { FC } from "react";
+import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/navigation";
 
 // CSS
-import '@/styles/portfolio/portfolio-hero.css';
+import "@/styles/portfolio/portfolio-hero.css";
 
 const PortfolioHero: FC = () => {
-  const t = useTranslations('PortfolioHero');
+  const t_Portfolio_PortfolioHero = useTranslations("Portfolio.PortfolioHero");
+  const t_Common = useTranslations("Common");
+
+  const { headline, subheadline, exploreOurWorksBtn, videoSRTxt } = {
+    headline: t_Portfolio_PortfolioHero("headline"),
+    subheadline: t_Portfolio_PortfolioHero("subheadline"),
+    exploreOurWorksBtn: t_Portfolio_PortfolioHero("exploreOurWorksBtn"),
+    videoSRTxt: t_Portfolio_PortfolioHero("videoSRTxt"),
+  };
+
+  const { bookASessionBtn, videoFallbackTxt } = {
+    bookASessionBtn: t_Common("bookASessionBtn"),
+    videoFallbackTxt: t_Common("videoFallbackTxt"),
+  };
 
   return (
-    <section className="portfolio-hero">
+    <section
+      className="portfolio-hero"
+      id="portfolio-hero"
+      aria-labelledby="portfolio-hero-headline"
+    >
       <video
-        className="portfolio-hero-video"
+        className="portfolio-hero-bg-video"
         aria-hidden="true"
         autoPlay
         loop
         muted
         playsInline
         preload="auto"
-        poster="/images/portfolio-video-placeholder.jpg"
+        poster="/videos/hero-bg/portfolio-hero-bg/portfolio-hero-bg.png"
       >
-        {/* <source src="/videos/portfolio-hero-bg.webm" type="video/webm" /> */}
-        <source src="/videos/portfolio-hero-bg.mp4" type="video/mp4" />
-        <span className="sr-only">
-          A showcase of a few of our best photography and videography scenes
-        </span>
-        Oops! It looks like your browser doesn&apos;t support video playback.
-        Don&apos;t worry - you can still explore our stunning visuals below!
+        <source
+          src="/videos/hero-bg/portfolio-hero-bg/portfolio-hero-bg.webm"
+          type="video/webm"
+        />
+        <source
+          src="/videos/hero-bg/portfolio-hero-bg/portfolio-hero-bg.mp4"
+          type="video/mp4"
+        />
+        <p className="sr-only">{videoSRTxt}</p>
+        {videoFallbackTxt}
       </video>
 
-      <div className="portfolio-hero-container">
-        <div className="portfolio-hero-content">
-          <h1 className="portfolio-hero-headline">{t('headline')}</h1>
-          <h2 className="portfolio-hero-subheadline">{t('subheadline')}</h2>
+      <div className="portfolio-hero-content">
+        <div className="portfolio-hero-container">
+          <h1 className="portfolio-hero-headline" id="portfolio-hero-headline">
+            {headline}
+          </h1>
+          <h2 className="portfolio-hero-subheadline">{subheadline}</h2>
           <div className="portfolio-hero-cta">
-            <a
-              href="/#portfolio-category"
-              className="explore-our-work-btn"
-              aria-label="Explore our portfolio"
+            <Link
+              href="/#portfolio-categories"
+              className="explore-our-works-btn"
+              aria-label={exploreOurWorksBtn}
+              prefetch
             >
-              {t('explore')}
-            </a>
+              {exploreOurWorksBtn}
+            </Link>
+
             <Link
               href="/contact-us"
               className="book-a-session-btn"
-              aria-label="Book a Session"
+              aria-label={bookASessionBtn}
+              prefetch
             >
-              {t('book')}
+              {bookASessionBtn}
             </Link>
           </div>
         </div>
