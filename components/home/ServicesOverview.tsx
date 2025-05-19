2f4d0d75004ad6a32786a8db429c6a2b37c23afb
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2025-05-19 12:17:19 +0600

    Services Overview Draft

--- components/home/ServicesOverview.tsx
+++ components/home/ServicesOverview.tsx
@@ -1,15 +1,69 @@
 import React, { FC } from "react";
-
+import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/navigation";
 // CSS
 import "@/styles/home/services-overview.css";
 
 const ServicesOverview: FC = () => {
+  const t_Section = useTranslations("Home.ServicesOverview");
+  const t_Common = useTranslations("Common");
+
+  const sectionHeading = t_Section("sectionHeading");
+  const sectionDescription = t_Section("sectionDescription");
+  const services = {
+    weddingsQuinceañeras: {
+      heading: t_Section("services.weddingsQuinceañeras.heading"),
+      subheading: t_Section("services.weddingsQuinceañeras.subheading"),
+      description: t_Section("services.weddingsQuinceañeras.description"),
+    },
+    musicArtistsBTS: {
+      heading: t_Section("services.musicArtistsBTS.heading"),
+      subheading: t_Section("services.musicArtistsBTS.subheading"),
+      description: t_Section("services.musicArtistsBTS.description"),
+    },
+    eventsCorporateCoverage: {
+      heading: t_Section("services.eventsCorporateCoverage.heading"),
+      subheading: t_Section("services.eventsCorporateCoverage.subheading"),
+      description: t_Section("services.eventsCorporateCoverage.description"),
+    },
+    personalFashionPortraits: {
+      heading: t_Section("services.personalFashionPortraits.heading"),
+      subheading: t_Section("services.personalFashionPortraits.subheading"),
+      description: t_Section("services.personalFashionPortraits.description"),
+    },
+    brandStoryTellingGastronomy: {
+      heading: t_Section("services.brandStoryTellingGastronomy.heading"),
+      subheading: t_Section("services.brandStoryTellingGastronomy.subheading"),
+      description: t_Section(
+        "services.brandStoryTellingGastronomy.description"
+      ),
+    },
+    childrenStudioPortraits: {
+      heading: t_Section("services.childrenStudioPortraits.heading"),
+      subheading: t_Section("services.childrenStudioPortraits.subheading"),
+      description: t_Section("services.childrenStudioPortraits.description"),
+    },
+  };
+  const exploreOurPortfolioBtn = t_Section("exploreOurPortfolioBtn");
+  const bookASessionBtn = t_Common("bookASessionBtn");
+
   return (
     <section
-      className="services-overview w-full h-96 bg-black text-white"
-      id="services-overview"
+      className="services-overview"
+      aria-labelledby="services-overview-heading"
     >
-      Services Overview
+      <div className="services-overview-container">
+        <h2
+          id="services-overview-heading"
+          className="services-overview-heading"
+        >
+          {sectionHeading}
+        </h2>
+
+        <p className="services-overview-description">{sectionDescription}</p>
+
+        <div className="services-overview-services"></div>
+      </div>
     </section>
   );
 };
